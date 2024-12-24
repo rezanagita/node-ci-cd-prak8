3e40ae35a60782d05320773abac2876c7e41193a
REZA NAGITA <rezagitta@com>
2024-12-25 05:47:24 +0700

    update lagi 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
- const app = express();
- app.get('/', (req, res) => {
- res.send('Hello, World!');
- });
- module.exports = app;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+module.exports = app; // Export the app
