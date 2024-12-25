13c558f67d795a8214d9e94b1ce3f2a1269dc82f
REZA NAGITA <rezagitta@com>
2024-12-25 07:02:01 +0700

    change

--- app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./app');
-describe('GET /', () => {
-    it('responds with Hello, World!', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello, World!');
-    });
-});
